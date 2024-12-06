44628883df602e51a71337cb74cf2eba99c63f79
UtkarshLB216 <utkarsh2212gonda@gmail.com>
2024-12-06 21:10:27 +0530

    [any message]

--- app/main.c
+++ app/main.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 100
+#define MAX_ARGS 10
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    char input[MAX_INPUT];
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        // Remove trailing newline
+        input[strcspn(input, "\n")] = '\0';
+
+        // Tokenize input
+        char *args[MAX_ARGS];
+        int i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && i < MAX_ARGS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process
+            execvp(args[0], args);
+            // If execvp returns, there was an error
+            printf("%s: not found\n", args[0]);
+            _exit(1);
+        } else if (pid > 0) {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+        } else {
+            // Fork failed
+            perror("fork");
+        }
+    }
+    return 0;
 }
